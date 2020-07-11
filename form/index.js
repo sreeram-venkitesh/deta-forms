ad5319c25e2b8adce326e131081f8be6783d1923
fillerInk <thatonephysicsnerd@gmail.com>
2020-07-11 19:00:05 +0530

    Install ejs

--- form/index.js
+++ form/index.js
@@ -4,5 +4,12 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+const PORT = 3000
+
+if(!process.env.DETA_RUNTIME){
+    app.listen(PORT,()=>{
+        console.log('Listening to local port')
+    })
+}
 // export 'app'
 module.exports = app
